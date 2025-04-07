fc88ad30e02e5b12e974f719a8d1c47a43d47bad
Jass-2 <jashankumarcw@gmail.com>
2025-04-07 05:31:12 -0400

    Update timeline/victoria medals page
    in this commit I Updated timeline/victoria medals page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
 import { setupMobileMenu } from './modules/hamburger.js';
 import { setupBattlefieldTabs } from './modules/slider.js';
 import { setupDonation } from './modules/donation.js';
-// import { createPopup } from './modules/popup.js';
+import { setupVictoriaSlider } from './modules/victoria.js';
+import { setupTimelineSlider } from './modules/timeline.js';
+import { initializePage } from './modules/news.js';
+
 
 // import { setupTransparentHeader } from './modules/transparentHeader.js';
 
@@ -19,6 +22,11 @@ import { setupDonation } from './modules/donation.js';
 
    setupDonation();
 
+   setupVictoriaSlider();
+
+   setupTimelineSlider();
+
+   initializePage();
   // Set up popup for donation
   // createPopup();
   // Set up transparent header
