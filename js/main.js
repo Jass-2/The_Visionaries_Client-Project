cda8e1db600fb15e419ba922d2dc9a1ec2a6e624
Jass-2 <jashankumarcw@gmail.com>
2025-04-09 03:57:35 -0400

    added sldier in history page, timelin html, victoria html
    history slider needs update, so will make another commit after advancements

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 
 // Import modules
 import { setupMobileMenu } from './modules/hamburger.js';
-import { setupBattlefieldTabs } from './modules/slider.js';
+// import { setupBattlefieldTabs } from './modules/slider.js';
 import { setupDonation } from './modules/donation.js';
-// import { createPopup } from './modules/popup.js';
-
-// import { setupTransparentHeader } from './modules/transparentHeader.js';
 import { heroAnimation, scrollAnimations, lenis } from "./modules/animations.js";
+<<<<<<< Updated upstream
 // Initialize on DOM content loaded
+=======
+import { setupBattlefieldSlider, setupLettersSlider } from './modules/history.js';
+>>>>>>> Stashed changes
 
 
-  // Set up mobile menu
-  setupMobileMenu();
+
+import { setupVictoriaSlider } from './modules/victoria.js';
+import { setupTimelineSlider } from './modules/timeline.js';
+import { initializePage } from './modules/news.js';
+
+
+   setupMobileMenu();
+
+   setupDonation();
+
+   setupVictoriaSlider();
+
+   setupTimelineSlider();
+
+   initializePage();
+   setupBattlefieldSlider();
+  
+   setupLettersSlider();
+
 
   // GSAP 
 
   heroAnimation();
   scrollAnimations();
   lenis ();
-  
-  // Set up battlefield tabs if they exist
-  setupBattlefieldTabs();
 
-
-  // Set up logo switch on scroll
-
-   setupDonation();
-
-  // Set up popup for donation
-  // createPopup();
-  // Set up transparent header
-  // setupTransparentHeader();
 
